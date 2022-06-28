c21f0da9515a645c9fbada0835cbf58c5ab7e4f4
Mindul <pencake33@naver.com>
2022-06-28 16:39:20 +0900

    로그인 문구 변경

--- src/main/java/uos/selab/controllers/SecurityController.java
+++ src/main/java/uos/selab/controllers/SecurityController.java
@@ -27,7 +27,7 @@ public class SecurityController {
     @PostMapping("/login")
     public String login(@RequestBody Map<String, String> user) {
         Member member = memberRepo.findById(user.get("id"))
-                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 아이디입니다."));
         /* if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
             throw new IllegalArgumentException("잘못된 비밀번호입니다.");
         } */
