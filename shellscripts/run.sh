4017070ae8d32a2ce0012a1dcfdecd366297cfda
Lurang <vmfoslkim@naver.com>
2022-07-22 12:20:48 +0900

    프로세스 시작 전 포트를 사용중인지 확인하여 사용중이라면 kill하도록 변경

--- shellscripts/run.sh
+++ shellscripts/run.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+pid_search=$(netstat -nap | grep 8083 | awk '{print $7}')
+pid_value=${#pid_search}
+
+if [ $pid_value == 0 ]; then
+    echo "Process already terminated."
+else
+    echo "Start terminate process"
+    pid_val=${pid_value%%'/'*}
+    kill -9 $pid_val
+    if [ $? ==0 ]; then
+        echo 'Terminate process success'
+    else
+        echo 'Terminate process fail'
+    fi
+fi
+
 if [ -d ./target ]; then
-    echo "/target is ready. run jar"
+    echo "/target is ready. Start jar"
     sudo nohup java -jar target/demo-1.0.0-DEPLOY.jar >> nohup.out 2>&1 &
 else
     echo "error: /target is not ready"
