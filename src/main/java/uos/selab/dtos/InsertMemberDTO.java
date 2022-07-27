ac21425e823570565277baec56872ccb2340cbee
Lurang <vmfoslkim@naver.com>
2022-07-27 10:46:33 +0900

    NotNull/email annotation에 메시지 추가

--- src/main/java/uos/selab/dtos/InsertMemberDTO.java
+++ src/main/java/uos/selab/dtos/InsertMemberDTO.java
@@ -19,20 +19,20 @@ public class InsertMemberDTO {
 
 	// private int memberNum;
 	
-	@NotNull
+	@NotNull(message = "id는 null일 수 없습니다.")
 	@Size(min = 1, max = 16, message = "id의 크기는 1에서 16 사이입니다.")
 	private String id;
 	
-	@NotNull
+	@NotNull(message = "password는 null일 수 없습니다.")
 	@Size(min = 1, max = 300, message = "password의 크기는 1에서 300 사이입니다.")
 	private String password;
 	
-	@NotNull
+	@NotNull(message = "nickname은 null일 수 없습니다.")
 	@Size(min = 1, max = 16, message = "nickname의 크기는 1에서 16 사이입니다.")
 	private String nickname;
 
-	@NotNull
-	@Email
+	@NotNull(message = "email은 null일 수 없습니다.")
+	@Email(message = "email의 형식이 맞지 않습니다.")
 	private String email;
 
 	// private Date createdAt;
